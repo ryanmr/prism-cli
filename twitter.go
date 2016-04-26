47b63a4e091d0af433e9b0427fc17abcb2da666e
Ryan Rampersad <ryan.rampersad@gmail.com>
2016-04-25 22:40:04 -0500

    Added get_api and show_tweets

--- twitter.go
+++ twitter.go
@@ -14,10 +14,44 @@ import (
   // "github.com/garyburd/go-oauth/oauth"
 )
 
-func show_tweets(api *TwitterApi) {
-  fmt.Println("show_tweets")
+func get_api() *anaconda.TwitterApi {
 
-  fmt.Println(api)
+  _, config, err := get_config()
+
+  if err != nil {
+    fmt.Println(`
+      The configuration could not be read.
+
+      Please run ./prism authorize.
+    `)
+  }
+
+  if !has_credentials(&config) {
+    fmt.Println(`
+      You have not authorized the app.
+
+      Please run ./prism authorize.
+    `)
+  }
+
+  anaconda.SetConsumerKey(config.ConsumerKey)
+  anaconda.SetConsumerSecret(config.ConsumerSecret)
+
+  api := anaconda.NewTwitterApi(config.AccessToken, config.AccessSecret)
+
+  return api
+}
+
+func show_tweets() {
+  
+  api := get_api()
+
+  tweets, _ := api.GetUserTimeline(nil)
+
+  for _, tweet := range tweets {
+      fmt.Println(tweet.Text)
+  }
+
 }
 
 // func get_tweets() {
