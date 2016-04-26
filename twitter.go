3850590e72e1d884bda009a3e1141edf85f24fbe
Ryan Rampersad <ryan.rampersad@gmail.com>
2016-04-25 19:05:09 -0500

    Added Twitter

---
+++ twitter.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  // "os"
+  // "net/http"
+  // "encoding/json"
+  // "io"
+  // "io/ioutil"
+  // "bufio"
+  // "strings"
+  // "github.com/codegangsta/cli"
+  "github.com/ChimeraCoder/anaconda"
+  // "github.com/garyburd/go-oauth/oauth"
+)
+
+func show_tweets(api *TwitterApi) {
+  fmt.Println("show_tweets")
+
+  fmt.Println(api)
+}
+
+// func get_tweets() {
+//
+// }
